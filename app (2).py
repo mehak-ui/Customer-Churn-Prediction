d557d75ad25bccd241831eb3288205e5b92e72e6
mehak-ui <137998664+mehak-ui@users.noreply.github.com>
2025-12-24 20:18:25 +0530

    Add files via upload

---
+++ app (2).py
@@ -0,0 +1,28 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+# Load your model & scaler (make sure these files exist in Colab)
+model = pickle.load(open("model.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
+
+
+st.title("Customer Churn Prediction")
+st.divider()
+st.write("Please enter the values and the hit the predict button for getting a prediction")
+st.divider()
+age = st.number_input("Enter Age", min_value=12, max_value=83, value=30)
+gender = st.selectbox("Select Gender",("Male","Female"))
+tenure = st.number_input("Enter Tenure", min_value=0, max_value=122, value=10)
+monthly_charges = st.number_input("Enter Monthly Charges", min_value=30.0, max_value=120.0, value=75.0)
+st.divider()
+predictbutton = st.button("Predict")
+if predictbutton:
+  gender_selection = 1 if gender == "Female" else 0
+  X = np.array([age,gender_selection,tenure,monthly_charges])
+  X_array = scaler.transform([X])
+  prediction = model.predict(X_array)[0]
+  predicted = "Yes" if prediction == 1 else "No"
+  st.write(f"Predicted: {predicted}")
+else:
+  st.write("Please hit the predict button")
